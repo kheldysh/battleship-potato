990ead2af09328cab6fffb9846c5b9e8dabc46bc
Svyatoslav Sobol <svyatoslav.sobol@relex.fi>
2017-06-15 14:33:00 +0300

    basic changes

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 var express = require('express');
+var range = require('lodash/range');
 var app = express();
 
+var grid;
+var row = [null, null, null, null, null];
+var initialGrid = [].concat(row).map(function() {
+    return [].concat(row);
+});
+
+
+var ships = {
+  carrier: 5,
+  battleship: 4,
+  cruiser: 3,
+  submarine: 3,
+  destroyer: 3
+};
+
 app.get('/start_game', function (req, res) {
-    res.json({ someResponse: 1 });
+    grid = initialGrid;
+    res.json({ someResponse: initialGrid });
 });
 
 app.post('/next_turn', function (req, res) {
