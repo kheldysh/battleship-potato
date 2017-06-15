ae8106519ebe8b22c38ccea78613f48ab4c32cbc
Jonas Öhman <jonas.ohman@relex.fi>
2017-06-15 16:16:17 +0300

    Updated randomizer

--- randomizer.js
+++ randomizer.js
@@ -2,16 +2,28 @@
  * Created by jonasohman on 15/06/2017.
  */
 var getRandomCoord = function (shootingHistory) {
-
-    var coord = randomize();
-
-    while (shootingHistory[coord.x, coord.y] !== NaN) {
-        coord = randomize();
+    var x = randomizeNumber();
+    while (!canShoot(shootingHistory[x])) {
+        x = randomizeNumber();
     }
-
-    return coord;
+    var y = randomizeNumber();
+    while(shootingHistory[x][y] !== NaN) {
+        y = randomizeNumber();
+    }
+    return {x: x, y: y};
 };
 
-var randomize = function () {
-    return {x: Math.floor(Math.random() * 9), y: Math.floor(Math.random() * 9)} ;
-}
+var randomizeNumber = function () {
+    var min = Math.ceil(0);
+    var max = Math.floor(10);
+    return Math.floor(Math.random() * (max - min)) + min;;
+};
+
+var canShoot = function (column) {
+    for(var y in column) {
+        if (y === NaN) {
+            return true;
+        }
+    }
+    return false;
+};
