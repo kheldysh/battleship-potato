5d8873d98af8171cab05ce9d148997926eb12ab6
Svyatoslav Sobol <svyatoslav.sobol@relex.fi>
2017-06-15 13:54:58 +0300

    api methods mocks

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.get('/start_game', function (req, res) {
+    res.json({ someResponse: 1 });
+});
+
+app.post('/next_turn', function (req, res) {
+    res.json({ nextTurn: 1 });
+});
+
+app.post('/end_game', function (req, res) {
+    res.json({ endGame: 1 });
 });
 
 app.listen(3000, function () {
